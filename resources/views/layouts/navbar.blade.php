ddca8cf9310448c73d9a7776ec10baaacf9f41da
Lqmnlhkim15 <luqmanlhkim15@gmail.com>
2024-04-27 13:27:35 +0700

    layout

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,19 @@
+<!-- nav -->
+{{-- Section Navbar Start  --}}
+<nav class="fixed top-0 left-0 right-0 bg-white z-50 shadow-md">
+    <div class="w-full">
+        <div class="flex relative justify-between">
+            <div class="py-3 px-10 flex gap-x-10 flex-2">
+                <h1 class="text-3xl text-primary font-bold">JAVAS <span class="text-black font-bold">MMS</span></h1>
+                <div class="w-10 h-10 rounded-full bg-slate-200">
+                    <div class="flex justify-center mt-2"> <img src="images/list.svg" alt="#" class="w-6"></i></div>
+                </div>
+            </div>
+            
+        <div class="flex items-center px-6">
+            <div class="flex relaive px-16"><img src="images/bell.svg" alt="#" class="w-6"></i></div>
+            <div class="profile w-10 relative h-10 rounded-full bg-slate-400 bg-cover bg-center bg-no-repeat" style="background-image: url(https://source.unsplash.com/featured?profile);"></div>
+        </div>
+    </div>
+</nav>
+{{-- Section Navbar End  --}}
