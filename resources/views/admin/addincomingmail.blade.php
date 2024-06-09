0770df543c12c2d34a9b64165f209804d17bc769
Lqmnlhkim15 <luqmanlhkim15@gmail.com>
2024-06-09 15:08:53 +0700

    commit

--- resources/views/admin/addincomingmail.blade.php
+++ resources/views/admin/addincomingmail.blade.php
@@ -12,7 +12,7 @@
             <label for="incomingLetter" class="mb-2">Unggah Surat Masuk</label>
             <input type="file" name="fileInput" class="dropIncomingMail" id="incomingLetter">
         </div>
-        <!-- < id="dropzone" class="mb-6">
+        {{-- <div id="dropzone" class="mb-6">
             <div
                 class="relative border-dashed border-4  border-primary h-72 flex justify-center items-center rounded-md bg-primary bg-opacity-5">
                 <label for="fileInput" class="text-center cursor-pointer">
@@ -25,43 +25,41 @@
                 </label>
             </div>
             <div id="fileList" class="text-center flex gap-3 flex-wrap"></div>
-        <div class="flex justify-end gap-4">
-            <a href="index.html" class="px-4 py-2 border-line border text-md rounded-md text-grey font-semibold">Kembali</a>
-            <button id="buttonPreview"
-                class="px-6  py-2 border-line border text-md rounded-md bg-primary text-white font-semibold">Simpan
-                Pesan Masuk</button>
-        </div>
+            <div class="flex justify-end gap-4">
+                <a href="index.html"
+                    class="px-4 py-2 border-line border text-md rounded-md text-grey font-semibold">Kembali</a>
+                <button id="buttonPreview"
+                    class="px-6  py-2 border-line border text-md rounded-md bg-primary text-white font-semibold">Simpan
+                    Pesan Masuk</button>
+            </div>
+        </div> --}}
     </form>
+    {{-- @push(script)
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $('.dropIncomingMail').dropify({
+                    tpl: {
+                        message: '<div class="dropify-message"><span class="file-icon" /> <p style="font-size: 24px;">Drag and drop a file here or click</p></div>',
+                    }
+                })
+                $('.dropInMail').dropify({
+                    tpl: {
+                        message: '<div class="dropify-message"><span class="file-icon" /> <p style="font-size: 24px;">Drag and drop a file here or click</p></div>',
+                    }
+                });
 
-
-        $(document).ready(function() {
-            $('.dropIncomingMail').dropify({
-                tpl: {
-                    message: '<div class="dropify-message"><span class="file-icon" /> <p style="font-size: 24px;">Drag and drop a file here or click</p></div>',
-                }
+                $('.tanggal').pickadate({
+                    labelMonthNext: 'Bulan Selanjutnya',
+                    labelMonthPrev: 'Bulan Sebelumnya',
+                    labelMonthSelect: 'Pilih Bulan',
+                    labelYearSelect: 'Pilih Tahun',
+                    selectMonths: true,
+                    selectYears: true,
+                    format: 'dddd, dd mmm yyyy',
+                    formatSubmit: 'yyyy/mm/dd',
+                    hiddenPrefix: 'prefix__',
+                    hiddenSuffix: '__suffix'
+                });
             })
-        })
-    </script>
-    <!-- <script type="text/javascript">
-        $(document).ready(function() {
-            $('.dropInMail').dropify({
-                tpl: {
-                    message: '<div class="dropify-message"><span class="file-icon" /> <p style="font-size: 24px;">Drag and drop a file here or click</p></div>',
-                }
-            });
-
-            $('.tanggal').pickadate({
-                labelMonthNext: 'Bulan Selanjutnya',
-                labelMonthPrev: 'Bulan Sebelumnya',
-                labelMonthSelect: 'Pilih Bulan',
-                labelYearSelect: 'Pilih Tahun',
-                selectMonths: true,
-                selectYears: true,
-                format: 'dddd, dd mmm yyyy',
-                formatSubmit: 'yyyy/mm/dd',
-                hiddenPrefix: 'prefix__',
-                hiddenSuffix: '__suffix'
-            });
-        })
-    </script> -->
-@endpush
+        </script> -->
+    @endpush --}}
